a1306bc6653381d46c9d6f0a4b7ac48c3ebda8e7
Ayushi Mittal <ayushi.mittal1@tothenew.com>
2023-09-08 19:09:03 +0530

    code with ssr react 18

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { AppProvider } from "./context/ProductContext";
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 // Slide - 3 : ReactDOM.createRoot instead of ReactDOM.render
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root =  ReactDOM.hydrateRoot(document.getElementById('root'));
 
 root.render(
     <AppProvider>
