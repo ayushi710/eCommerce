16be11655ea708a88058f382d33e689e97173f27
Ayushi Mittal <ayushi.mittal1@tothenew.com>
2023-09-16 18:31:04 +0530

    changes in contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -33,9 +33,44 @@ const Contact = () => {
     }
   `;
 
-  return <Wrapper>
-    <h1>Contact Page</h1>
-  </Wrapper>;
+  return  <Wrapper>
+  <h2 className="common-heading">Contact page</h2>
+  <div className="container">
+    <div className="contact-form">
+      <form
+        method="POST"
+        className="contact-inputs">
+        <input
+          type="text"
+          placeholder="username"
+          name="username"
+          //value={isAuthenticated ? user.name : ""}
+          required
+          autoComplete="off"
+        />
+
+        <input
+          type="email"
+          name="Email"
+          placeholder="Email"
+          autoComplete="off"
+          //value={isAuthenticated ? user.email : ""}
+          required
+        />
+
+        <textarea
+          name="Message"
+          cols="30"
+          rows="10"
+          required
+          autoComplete="off"
+          placeholder="Enter you message"></textarea>
+
+        <input type="submit" value="send" />
+      </form>
+    </div>
+  </div>
+</Wrapper>;
 };
 
 export default Contact;
